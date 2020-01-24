1d5ab1c68a27d730fe716e496f8832090042b91f
Udam Liyanage <udam1998@gmail.com>
2020-01-25 00:23:44 +0530

    MQTT Listener

--- main.go
+++ main.go
@@ -2,14 +2,40 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
-func OnMessageReceived(client MQTT.Client, message MQTT.Message) {
+func OnMessageReceived(_ MQTT.Client, message MQTT.Message) {
 	fmt.Printf("Received message on topic: %s\nMessage: %s\n", message.Topic(), message.Payload())
 }
 
 func main() {
-	println("Main Function")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	opts := setClientOptions()
+	topic := "/test-topic"
+
+	opts.OnConnect = func(c MQTT.Client) {
+		if token := c.Subscribe(topic, 0, OnMessageReceived); token.Wait() && token.Error() != nil {
+			panic(token.Error())
+		}
+	}
+	client := MQTT.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	} else {
+		fmt.Printf("Connected to server:\n")
+	}
+	<-c
+}
+
+func setClientOptions() *MQTT.ClientOptions {
+	opts := MQTT.NewClientOptions().AddBroker("tcp://54.67.77.75:1883")
+	opts.SetDefaultPublishHandler(OnMessageReceived)
+	return opts
 }

3010b62029747469ecdc1c04682ea9695bf523e8
Udam Liyanage <udam1998@gmail.com>
2020-01-25 00:15:14 +0530

    Message receive action stub

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+func OnMessageReceived(client MQTT.Client, message MQTT.Message) {
+	fmt.Printf("Received message on topic: %s\nMessage: %s\n", message.Topic(), message.Payload())
+}
+
 func main() {
 	println("Main Function")
 }
